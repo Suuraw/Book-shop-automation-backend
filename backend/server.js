6d1ee7dbf6fdd75bab78b76a9c088c992f6ebba4
Sujay kumar <sujayraw13@gmail.com>
2024-08-21 19:20:12 +0530

    Sign page and login page logic is done!

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
 
-// Set up the database connection
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -11,7 +11,6 @@ const db = new pg.Client({
   port: "5432",
 });
 
-// Connect to the database
 db.connect();
 
 const app = express();
@@ -20,45 +19,62 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Initialize an empty array to hold user data
-let Data = [];
-
-// Function to fetch user data from the database
-const fetchUserData = async () => {
+const checkExistingUser = async (username) => {
   try {
-    const res = await db.query("SELECT * FROM users");
-    Data = res.rows;
-    console.log("User Data:", Data);
+    const res = await db.query("SELECT * FROM users WHERE username=$1", [
+      username,
+    ]);
+
+    if (res.rows.length > 0)
+      //user exist
+      return true;
+    else return false;
   } catch (err) {
-    console.error("Error executing the query:", err.stack);
-    // Set Data to an empty array if an error occurs to avoid further issues
-    Data = [];
+    console.log(err);
+    return false;
   }
-  db.end()
 };
 
-// Fetch user data on server startup
-fetchUserData();
-
-app.get("/", (req, res) => {
-  return res.json({ message: "Server is running fine" });
-});
-
-app.post("/auth", (req, res) => {
-  const { username } = req.body;
-
-  const find = Data.find((user) => user.name === username);
-
-  if (find) {
-    return res.status(200).json({ message: "User exists" });
-  } else {
-    return res.status(404).json({ message: "User doesn't exist" });
+app.post("/Signup", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const users = await checkExistingUser(username);
+    if (users) {
+      return res.status(409).json({ message: "Username already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.query("INSERT INTO users (username,password) VALUES($1,$2)", [
+      username,
+      hashedPassword,
+    ]);
+    return res.status(201).json({ message: "Signed up Successfull" });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
   }
 });
 
-// Close the database connection when the server is shut down
-  
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
+  try {
+    const result = await db.query("SELECT * FROM users WHERE username=$1", [
+      username,
+    ]);
+    if (result.rows.length == 0) {
+      return res.status(409).json({message:"Invalid Username"})
+    }
+      const user = result.rows[0];
+      const validPassword = await bcrypt.compare( password,user.password);
+      if (validPassword) {
+        return res.status(200).json({ message: "Login Successfull" });
+      } else {
+        return res.status(409).json({ message: "Invalid Password" });
+      }
+    }
+   catch (error) {
+    return res.status(500).json("Server Not responding")
+  }
+});
 
 app.listen(port, () => {
   console.log(`The server is listening on port ${port}`);
